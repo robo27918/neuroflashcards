29b40e468f8a1cae32c68833fdadd71451bdf737
Roberto Toribio <roberttoribio04@gmail.com>
2023-09-22 13:50:02 -0700

    add brain anatomy data

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,18 @@
  * for holding and rendering the individuals cards
  */
 import React from "react";
-//import Cards from "./Cards";
+import brainAnatomyFlashcards from "./cardData";
 import CardDeck from "./components/CardDeck";
 import "./App.css"
 
-const flashCards =[
-  {
-    id:1,
-    front:"question1",
-    back:"answer1"
-  },
-  {
-    id:2,
-    front:"question2",
-    back:"answer2"
-  },
-  {
-    id:3,
-    front:"question3",
-    back:"answer3"
-  }
-]
+
 function App() {
   
 
   return (
     <div>
       {/*This component will be in charge of rendering the deck of flash cards */}
-     <CardDeck cards={flashCards}/>
+     <CardDeck cards={brainAnatomyFlashcards}/>
     </div>
   )
 }
---
+++ src/cardData.jsx
@@ -0,0 +1,53 @@
+const brainAnatomyFlashcards = [
+    {
+      id: 1,
+      front: "What is the largest part of the human brain responsible for higher-order thinking and voluntary muscle movements?",
+      back: "Cerebrum"
+    },
+    {
+      id: 2,
+      front: "Which part of the brain is responsible for regulating vital functions such as breathing, heart rate, and blood pressure?",
+      back: "Brainstem"
+    },
+    {
+      id: 3,
+      front: "What is the term for the outermost layer of the cerebrum that is responsible for conscious thought and decision-making?",
+      back: "Cerebral Cortex"
+    },
+    {
+      id: 4,
+      front: "Which part of the brain connects the two hemispheres and allows them to communicate with each other?",
+      back: "Corpus Callosum"
+    },
+    {
+      id: 5,
+      front: "What is the name of the small, almond-shaped structure in the brain that plays a key role in processing emotions, especially fear?",
+      back: "Amygdala"
+    },
+    {
+      id: 6,
+      front: "Which part of the brain is responsible for coordinating muscle movements, balance, and posture?",
+      back: "Cerebellum"
+    },
+    {
+      id: 7,
+      front: "What is the fluid-filled space within the brain that provides cushioning and support for the brain and spinal cord?",
+      back: "Cerebrospinal Fluid (CSF)"
+    },
+    {
+      id: 8,
+      front: "Which part of the brain is involved in regulating sleep and wake cycles, as well as circadian rhythms?",
+      back: "Pineal Gland"
+    },
+    {
+      id: 9,
+      front: "What is the name of the region in the brain responsible for processing sensory information and relaying it to the appropriate areas of the cerebral cortex?",
+      back: "Thalamus"
+    },
+    {
+      id: 10,
+      front: "Which part of the brain is responsible for controlling basic survival functions like hunger, thirst, and body temperature?",
+      back: "Hypothalamus"
+    }
+  ];
+  export default brainAnatomyFlashcards
